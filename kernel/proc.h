c6f5d21c0202d2ad8be12d0454f8751cea81c9f3
lisnail <lisnail@1659765830@qq.com>
2023-08-18 11:53:53 +0800

    完成第六次任务

--- kernel/proc.h
+++ kernel/proc.h
@@ -1,6 +1,6 @@
 // Saved registers for kernel context switches.
 struct context {
-  uint64 ra;
+  uint64 ra;//返回地址
   uint64 sp;
 
   // callee-saved
@@ -17,6 +17,8 @@ struct context {
   uint64 s10;
   uint64 s11;
 };
+
+
 
 // Per-CPU state.
 struct cpu {
@@ -102,6 +104,7 @@ struct proc {
   pagetable_t pagetable;       // User page table
   struct trapframe *trapframe; // data page for trampoline.S
   struct context context;      // swtch() here to run process
+
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
